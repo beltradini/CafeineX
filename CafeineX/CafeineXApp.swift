94b4327ea3829a550f268f17ae97d90fae9a7ed9
alex beltrannn <alexbeltrn@outlook.com>
2026-05-24 16:03:34 -0600

    Initial Commit

---
+++ CafeineX/CafeineXApp.swift
@@ -0,0 +1,32 @@
+//
+//  CafeineXApp.swift
+//  CafeineX
+//
+//  Created by Alejandro Beltrán on 5/24/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CafeineXApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CafeineXTests/CafeineXTests.swift
@@ -0,0 +1,19 @@
+//
+//  CafeineXTests.swift
+//  CafeineXTests
+//
+//  Created by Alejandro Beltrán on 5/24/26.
+//
+
+import Testing
+@testable import CafeineX
+
+struct CafeineXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
